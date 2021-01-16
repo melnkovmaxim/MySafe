edca451aa2fcd69e338cfb82d56036780c7e41c2
Melnikov Maxim <melnikovmaxim@icloud.com>
2021-01-16 19:36:23 +1000

    add models

---
+++ MySafe/MySafe/Models/DiskInfo/DiskInfo.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel;
+using Newtonsoft.Json;
+using Prism.Mvvm;
+using PropertyChanged;
+
+namespace MySafe.Models.DiskInfo
+{
+    [JsonObject]
+    public class DiskInfo : BindableBase
+    {
+        [JsonProperty("max_file_size")]
+        public long MaxFileSize { get; set; }
+
+        [JsonProperty("unlimited_autoupload_enabled")]
+        public bool Unlimited { get; set; }
+
+        [JsonProperty("total_space")]
+        public long TotalSpace { get; set; }
+
+        [JsonProperty("trash_size")]
+        public long TrashSize { get; set; }
+
+        [JsonProperty("is_paid")]
+        public bool IsPaid { get; set; }
+
+        [JsonProperty("used_space")]
+        public long UsedSpace { get; set; }
+
+        [JsonProperty("user")]
+        public UserInfo User { get; set; }
+    }
+}
---
+++ MySafe/MySafe/Models/Request/Images/ImagesRotateRequest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace MySafe.Models.Request.Images
+{
+    [JsonObject]
+    public class ImagesRotateRequest : IToken
+    {
+        public string AccessToken { get; set; }
+
+        [JsonProperty("rotate")]
+        public char Rotate { get; set; }
+    }
+}
