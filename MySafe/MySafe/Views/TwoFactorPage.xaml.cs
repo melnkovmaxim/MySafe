d311aabd7dc1bf4a7a8fa22a7c8f7b167ebbd796
Melnikov Maxim <melnikovmaxim@icloud.com>
2021-03-02 00:02:35 +1000

    fix

--- MySafe/MySafe/Views/TwoFactorPage.xaml.cs
+++ MySafe/MySafe/Views/TwoFactorPage.xaml.cs
@@ -11,11 +11,20 @@ using Xamarin.Forms.Xaml;
 namespace MySafe.Views
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
-    public partial class TwoFactorPage : ContentPage
+    public partial class TwoFactorPage : ContentPage, INavigatedAware
     {
         public TwoFactorPage()
         {
             InitializeComponent();
+        }
+
+        public void OnNavigatedFrom(INavigationParameters parameters)
+        {
+            Navigation.NavigationStack.ToList().ForEach(Navigation.RemovePage);
+        }
+
+        public void OnNavigatedTo(INavigationParameters parameters)
+        {
         }
     }
 }
