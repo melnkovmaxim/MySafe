818f1faad78579a9771c12726073f3af57513b08
Melnikov Maxim <melnikovmaxim@icloud.com>
2021-03-12 23:30:42 +1000

    refreshview spinner

--- MySafe/MySafe/Views/MainPage.xaml.cs
+++ MySafe/MySafe/Views/MainPage.xaml.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using ImTools;
 using Prism.Navigation;
 using Xamarin.Forms;
 using Xamarin.Forms.Internals;
@@ -14,13 +17,28 @@ namespace MySafe.Presentation.Views
         {
             NavigationPage.SetHasNavigationBar(this, false);
             InitializeComponent();
-
         }
 
         private void Blue_Button_Clicked(object sender, EventArgs e)
         {
             Button button = (Button)sender;
             button.BackgroundColor = Color.Blue;
+        }
+
+        private void RefreshView_OnRefreshing(object sender, EventArgs e)
+        {
+            var cancellationTokenSource = new CancellationTokenSource();
+            var token = cancellationTokenSource.Token;
+
+            Task.Run(async () =>
+            {
+                while (token.IsCancellationRequested == false)
+                {
+                    await _spinnetImage.RelRotateTo(360, 1000); 
+                }
+            }, token);
+
+            cancellationTokenSource.Cancel();
         }
     }
 }
