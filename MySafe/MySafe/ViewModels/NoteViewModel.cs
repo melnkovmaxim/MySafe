668ac33921d86ec81362e283d1ff372256004ee2
Melnikov Maxim <melnikovmaxim@icloud.com>
2021-03-14 14:21:26 +1000

    noteviewmodel changes

--- MySafe/MySafe/ViewModels/NoteViewModel.cs
+++ MySafe/MySafe/ViewModels/NoteViewModel.cs
@@ -1,12 +1,28 @@
-﻿using MySafe.Presentation.ViewModels.Abstractions;
+﻿using System.Collections.ObjectModel;
+using System.Threading.Tasks;
+using MediatR;
+using MySafe.Business.Mediator.Notes.NoteListQuery;
+using MySafe.Core.Entities.Responses;
+using MySafe.Presentation.ViewModels.Abstractions;
 using Prism.Navigation;
 
 namespace MySafe.Presentation.ViewModels
 {
-    public class NoteViewModel : ViewModelBase
+    public class NoteViewModel : AuthorizedRefreshViewModel<Note>
     {
-        public NoteViewModel(INavigationService navigationService) : base(navigationService)
+        private readonly IMediator _mediator;
+        public ObservableCollection<Note> Notes { get; set; }
+
+        public NoteViewModel(INavigationService navigationService, IMediator mediator) : base(navigationService)
         {
+            _mediator = mediator;
+        }
+
+        protected override Task<Note> _refreshTask => _mediator.Send(new NoteListQuery());
+        protected override void RefillObservableCollection(Note mediatorResponse)
+        {
+            /// Нужен акк Влада чтоб проверить резалтсет
+            Notes.Clear();
         }
     }
 }
