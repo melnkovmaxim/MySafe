1d58ddf9bfa9f29c87643e65687137676aa0825d
Melnikov Maxim <melnikovmaxim@icloud.com>
2021-03-15 22:10:55 +1000

    refactoring and pipe blanks

--- MySafe.Business/Mediator/RequestExceptionHandler.cs
+++ MySafe.Business/Mediator/RequestExceptionHandler.cs
@@ -4,16 +4,20 @@ using System.Linq;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using MediatR;
 using MediatR.Pipeline;
+using MySafe.Core.Entities.Responses.Abstractions;
 
 namespace MySafe.Business.Mediator.Pipelines
 {
-    public class RequestExceptionHandler<TRequest, TResponse>: IRequestExceptionHandler<TRequest, TResponse>
+    public class RequestExceptionHandler<TRequest, TResponse>: IRequestExceptionHandler<TRequest, TResponse> 
+        where TRequest   : IRequest
+        where TResponse  : IResponse
     {
         public Task Handle(TRequest request, Exception exception, RequestExceptionHandlerState<TResponse> state,
             CancellationToken cancellationToken)
         {
-            throw new Exception();
+            throw new NotImplementedException();
         }
     }
 }
